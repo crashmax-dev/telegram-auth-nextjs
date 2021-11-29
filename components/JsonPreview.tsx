6e7bd8d389300c7e11ac97f197404c3a4ccb6531
Vitalij Ryndin <sys@crashmax.ru>
2021-11-29 22:36:04 +0800

    update package:  `@crashmax/json-format-highlight`

--- components/JsonPreview.tsx
+++ components/JsonPreview.tsx
@@ -1,22 +1,13 @@
 import React from 'react'
-import formatJson from 'json-format-highlight'
+import formatJson from '@crashmax/json-format-highlight'
 
 const JsonPreview: React.FC = (props) => {
-  const colors = {
-    keyColor: '#9cdcfe',
-    nullColor: '#569cd6',
-    trueColor: '#569cd6',
-    falseColor: '#569cd6',
-    numberColor: '#b5cea8',
-    stringColor: '#ce9178'
-  }
-
-  const code = formatJson(props.children, colors)
+  const __html = formatJson(props.children)
 
   return (
     <pre
       className="max-w-5xl bg-gray text-white shadow-lg overflow-auto w-full rounded-md p-4"
-      dangerouslySetInnerHTML={{ __html: code.replace(/word-wrap:break-word;/g, '') }}
+      dangerouslySetInnerHTML={{ __html }}
     ></pre>
   )
 }
