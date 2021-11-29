48c0a79cb7b6ad264e10ba75d6dcd35989ba77f5
Vitalij Ryndin <sys@crashmax.ru>
2021-11-29 22:52:27 +0800

    add progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import type { AppProps } from 'next/app'
 import { SWRConfig } from 'swr'
 import fetcher from 'lib/fetcher'
+import Router from 'next/router'
+import ProgressBar from '@badrap/bar-of-progress'
 import 'tailwindcss/tailwind.css'
+
+const progress = new ProgressBar({
+  className: 'bg-telegram',
+  delay: 80,
+  size: 2
+})
+
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on('routeChangeError', progress.finish)
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
